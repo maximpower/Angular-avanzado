dfbb2dfed1755ce82f9f7f52cbcdfd3c20d3186d
Maksym Yuriy Nakonechny <maxim.n.dev@gmail.com>
2021-08-29 19:45:11 +0200

    Routing by modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PagesRoutingModules } from './pages/pages-routing.module';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { PagesModule } from './pages/pages.module';
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
   { path: '**', component: NopagefoundComponent },
 ];
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), PagesRoutingModules],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
